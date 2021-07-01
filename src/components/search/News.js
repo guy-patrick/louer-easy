4323f0544c2c31420074b322fe49ea3965a831d1
Patrick Ouambo <patgp04@gmail.com>
2021-07-01 06:55:50 +0100

    update css sass

---
+++ src/components/search/News.js
@@ -0,0 +1,42 @@
+export const News = () => (
+  <div className="editorials">
+    <div className="inside">
+      <h2>
+        <span>Les actualités </span>
+        <span style={{ fontWeight: 800 }}>de l’immobilier</span>
+        <span></span>
+      </h2>
+      <div className="posts-container">
+        <div className="posts-container-inner">
+          <div className="post">
+            <a href="https://edito.seloger.com/conseils-d-experts/reglementations/imposee-plus-value-realisee-une-sci-article-43665.html">
+              <img
+                alt="<p>Le régime applicable aux plus-values immobilières réalisées occasionnellement par l’intermédiaire d’une SCI suit le même régime qu’une plus-value réalisée par un particulier.</p>
+"
+                src="https://edito.seloger.com/sites/default/files/styles/manual_crop_306x198/public/article/image/vente-plus-value-sci-seloger.jpg?itok=9CnvRTKd"
+              />
+              <div>Réglementations</div>
+              <p data-ti="article-title">
+                Comment est imposée la plus-value réalisée par une SCI ?
+              </p>
+            </a>
+          </div>
+          <div className="post">
+            <a href="https://edito.seloger.com/conseils-d-experts/immobilier-local/l-ouest-l-francilien-attirent-acquereurs-d-une-residence-secondaire-interview-43641.html">
+              <img
+                alt="<p>Avec la crise sanitaire, la demande de résidences secondaires a augmenté en Île-de-France. Partons à la découverte de ce marché prisé avec Christine Pelosse, Directrice des agences Homelike Home, à Paris.</p>
+"
+                src="https://edito.seloger.com/sites/default/files/styles/manual_crop_306x198/public/article/image/residence-secondaire-ile-de-france-seloger.jpg?itok=ceXq4GWo"
+              />
+              <div>Immobilier local</div>
+              <p data-ti="article-title">
+                « L’ouest et l’est francilien attirent les acquéreurs d’une
+                résidence secondaire »
+              </p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,9 @@
 import { Search } from "../components/search/Search";
+import { News } from "../components/search/News";
 
 export const Homepage = () => (
   <div className="content">
     <Search />
+    <News />
   </div>
 );
